d10ab26790b7ac1717a36dee838eee675adadf36
Peter Svensson <peter@sparetimecoders.com>
2025-05-02 08:20:27 +0200

    feat: update-github-runner-addons (#19)
    * chore(doc): update README
    
    * feat: add listener template with resource requests
    
    Adds a new listener template in values.yaml and gha-runner-scale-set.yaml 
    to define resource requests for CPU and memory. Updates the output name 
    in outputs.tf to include runner name and group for better uniqueness. 
    These changes enhance resource management and improve clarity in output 
    naming.

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "addons" {
   value = [{
     content = local.yaml
     version = local.version
-    name    = "github-runners"
+    name    = "github-runners-${var.runner_name}-${var.runner_group}"
   }]
 }
